1a35d78c171b51c3b36c5a38c7ba976739500741
JhonatanDevv <jholxd@hotmail.com>
2025-01-27 06:39:35 -0300

    Alterações no projeto, Migrando dados episodio temporada para principal

--- src/main/java/br/com/alura/screenmatch2/screenmatch2/Screenmatch2Application.java
+++ src/main/java/br/com/alura/screenmatch2/screenmatch2/Screenmatch2Application.java
@@ -1,16 +1,12 @@
 package br.com.alura.screenmatch2.screenmatch2;
 
-import br.com.alura.screenmatch2.screenmatch2.model.DadosEpisodio;
-import br.com.alura.screenmatch2.screenmatch2.model.DadosSerie;
-import br.com.alura.screenmatch2.screenmatch2.model.DadosTemporada;
-import br.com.alura.screenmatch2.screenmatch2.service.ConsumoApi;
-import br.com.alura.screenmatch2.screenmatch2.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.alura.screenmatch2.screenmatch2.principal.Principal;
 
-import java.util.ArrayList;
-import java.util.List;
+
+
 
 @SpringBootApplication
 public class Screenmatch2Application implements CommandLineRunner {
@@ -21,24 +17,8 @@ public class Screenmatch2Application implements CommandLineRunner {
     }
     @Override
     public void run(String... args) throws Exception {
-        var consumoApi = new ConsumoApi();
-        var json = ConsumoApi.obterDados("https://www.omdbapi.com/?t=friends&apikey=98437a33");
-        System.out.println(json);
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
-        json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&season=1&episode=1&apikey=98437a33");
-        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-        System.out.println(dadosEpisodio);
-
-        List<DadosTemporada> temporadas = new ArrayList<>();
-
-        for (int i = 0; i <=dados.totalTemporadas(); i++) {
-            json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&season="+ i +"&apikey=98437a33");
-            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-            temporadas.add(dadosTemporada);
-        }
-        temporadas.forEach(System.out::println);
+        Principal principal = new Principal();
+        principal.exibeMenu();
 
     }
 
--- src/main/java/br/com/alura/screenmatch2/screenmatch2/model/DadosEpisodio.java
+++ src/main/java/br/com/alura/screenmatch2/screenmatch2/model/DadosEpisodio.java
@@ -3,8 +3,9 @@ package br.com.alura.screenmatch2.screenmatch2.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosEpisodio(@JsonAlias("Title") String title,
+public record DadosEpisodio(@JsonAlias("Title") String titulo,
                             @JsonAlias("Episode") Integer numero,
                             @JsonAlias("imdbRating") String avaliacao,
                             @JsonAlias("Released")  String dataLancamento) {
